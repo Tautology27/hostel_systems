4246aa3ef4281e6b53d9924518507dcb5beb1c73
baraka2003 <barakamulimila@gmail.com>
2025-05-12 09:08:22 +0300

    changes

--- db.php
+++ db.php
@@ -4,7 +4,8 @@ $user = "hostel_user";     // Same as in .env
 $pass = "hostelpass";      // Same as in .env
 $db   = "hostel_db";       // Same as in .env
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli("hostel-db", "hostel_user", "hostelpass", "hostel_db");
+
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
